dc1789be7fffb1d12a42294ca838dbaea1863c83
Santiago Cañu <santisv4@gmail.com>
2024-08-03 07:54:03 -0300

    verosa

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div>
-        <img className='logo-vo' src='logoVO.png'></img>
+        <img className='logo-vo' src='logoVO.png' alt='ve-rosa'></img>
       </div>
       <div className="footer-content">
         <p>&copy; 2024 Visión Online. Todos los derechos reservados.</p>
