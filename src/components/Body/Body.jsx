943521609a5327ca2a3d87801fdf4312c72272a9
Santiago Cañu <santisv4@gmail.com>
2025-03-13 23:05:17 -0300

    mm

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -25,7 +25,7 @@ export default function Body() {
 
   return (
     <section className='container-father-header'>
-
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
       <div className='left-side'>
     
         <div className={'container'}>
